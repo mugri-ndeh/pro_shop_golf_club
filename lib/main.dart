026e3a08813e379a1e5615b27f0c6d3aeb1944ce
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-15 23:32:20 +0100

    added providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pro_shop_golf_club/auth/auth_provider.dart';
 import 'package:pro_shop_golf_club/auth/login/login.dart';
 import 'package:pro_shop_golf_club/auth/onboarding/splash.dart';
+import 'package:provider/provider.dart';
 
 import 'util/constants/palette.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
+  Provider.debugCheckInvalidValueType = null;
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Palette.scaffoldBg,
       statusBarIconBrightness: Brightness.dark));
-  runApp(const MyApp());
+  runApp(MultiProvider(
+      providers: [Provider(create: (context) => Authentication())],
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ProClub shop',
+      title: 'GolfHub',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.green,
