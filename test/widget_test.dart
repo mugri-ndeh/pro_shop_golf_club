bd465c4a62a70bbe085176b0ebe4a35f2aeeec3b
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-26 04:33:55 +0100

    tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pro_shop_golf_club/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // await tester.pumpWidget(const ());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
