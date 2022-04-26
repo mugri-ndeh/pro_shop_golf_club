8dcdb1d4b645685bda4e09c377de53e04214e7b2
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-26 04:40:21 +0100

    few palette changes

--- lib/util/constants/palette.dart
+++ lib/util/constants/palette.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static Color primaryGreen = const Color(0XFF00B050);
-  static Color secondaryColor = const Color(0XFFF56677);
+  static Color secondaryColor = Color.fromARGB(255, 7, 221, 7);
+
   static Color scaffoldBg = const Color(0XFFF9F9F9);
   static Color borderGreen = const Color.fromARGB(255, 0, 17, 8);
   static Color white = Colors.white;
