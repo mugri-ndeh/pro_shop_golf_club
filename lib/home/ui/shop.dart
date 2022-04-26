0013d4ea8f083aa855eb47078efc2eced421742c
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-26 04:38:48 +0100

    shop page init

---
+++ lib/home/ui/shop.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Shop extends StatefulWidget {
+  Shop({Key? key}) : super(key: key);
+
+  @override
+  State<Shop> createState() => _ShopState();
+}
+
+class _ShopState extends State<Shop> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
