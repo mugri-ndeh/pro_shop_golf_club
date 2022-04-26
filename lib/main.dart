de1d25b2a94f8de91c9173e95bda399e3fd28fea
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-26 03:51:16 +0100

    removed unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pro_shop_golf_club/auth/auth_provider.dart';
-import 'package:pro_shop_golf_club/auth/login/login.dart';
 import 'package:pro_shop_golf_club/auth/onboarding/splash.dart';
 import 'package:provider/provider.dart';
 
@@ -12,76 +11,24 @@ import 'util/constants/palette.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform,
+      // options: DefaultFirebaseOptions.currentPlatform
       );
   Provider.debugCheckInvalidValueType = null;
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Palette.scaffoldBg,
       statusBarIconBrightness: Brightness.dark));
-  runApp(MultiProvider(
+  runApp(
+    MultiProvider(
       providers: [Provider(create: (context) => Authentication())],
-      child: const MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GolfHub',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.green,
-          scaffoldBackgroundColor: Palette.scaffoldBg,
-          textTheme: GoogleFonts.montserratTextTheme()),
-      home: Splash(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      child: MaterialApp(
+        title: 'GolfHub',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.green,
+            scaffoldBackgroundColor: Palette.scaffoldBg,
+            textTheme: GoogleFonts.montserratTextTheme()),
+        home: Splash(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+    ),
+  );
 }
