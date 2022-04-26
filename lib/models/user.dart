57b9f94ebca65404cc54c7a76673134100e1d352
mugri-ndeh <frunwindeh@yahoo.com>
2022-04-26 04:39:59 +0100

    user model almost there

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,4 +1,5 @@
 class UserModel {
+  bool completedProfile;
   String uid;
   String username;
   String firstName;
@@ -7,13 +8,48 @@ class UserModel {
   String phoneNumber;
   String dob;
   List? paymentMethods;
-  UserModel(
-      {this.uid = '',
-      this.username = '',
-      this.firstName = '',
-      this.lastName = '',
-      this.email = '',
-      this.dob = '',
-      this.phoneNumber = '',
-      this.paymentMethods});
+  List purchases;
+  UserModel({
+    this.uid = '',
+    this.username = '',
+    this.firstName = '',
+    this.lastName = '',
+    this.email = '',
+    this.dob = '',
+    this.phoneNumber = '',
+    this.paymentMethods,
+    this.purchases = const [],
+    this.completedProfile = false,
+  });
+  createUser() {}
+  updateInfo() {}
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      uid: json['uid'],
+      email: json['email'],
+      username: json['username'],
+      phoneNumber: json['phoneNumber'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      dob: json['dob'],
+      paymentMethods: json['paymentMethods'],
+      purchases: json['purchases'],
+      completedProfile: json['completedProfile'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'username': username,
+      'phoneNumber': phoneNumber,
+      'uid': uid,
+      'firstName': firstName,
+      'lastName': lastName,
+      'dob': dob,
+      'paymentMethods': paymentMethods,
+      'purchases': purchases,
+      'completedProfile': completedProfile
+    };
+  }
 }
