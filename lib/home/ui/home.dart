a6853c7b8ec9a365038623af867e6e6a5d5d748e
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-04 13:24:01 +0100

    set appbar to all screens

--- lib/home/ui/home.dart
+++ lib/home/ui/home.dart
@@ -28,192 +28,199 @@ class _HomeState extends State<Home> {
     var emoji = Emojis.wavingHandDarkSkinTone;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Palette.scaffoldBg,
-        elevation: 0,
-        leading: const Icon(
-          Icons.menu,
-          color: Colors.black,
-        ),
-        actions: const [
-          Icon(
-            Icons.notifications,
-            color: Colors.black,
-          ),
-          SizedBox(width: 5)
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 200,
-              child: Stack(children: [
-                Container(
-                  height: 130,
-                  decoration: BoxDecoration(
-                    color: Palette.scaffoldBg,
-                    borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        bottomRight: Radius.circular(20)),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: const Offset(0, 1),
-                          color: Palette.primaryGreen.withOpacity(0.09),
-                          blurRadius: 2),
-                    ],
+      body: Container(
+        height: size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 200,
+                child: Stack(children: [
+                  Container(
+                    height: 130,
+                    decoration: BoxDecoration(
+                      color: Palette.scaffoldBg,
+                      borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                            offset: const Offset(0, 1),
+                            color: Palette.primaryGreen.withOpacity(0.09),
+                            blurRadius: 2),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Hey ${auth.loggedUser?.username} $emoji',
+                                style: const TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              const Text('Let\'s play some golf today!',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  )),
+                            ],
+                          ),
+                          CircleAvatar(
+                            radius: 26,
+                            child: CircleAvatar(
+                                radius: 24,
+                                foregroundColor: Colors.transparent,
+                                backgroundColor: Palette.white,
+                                child: Image.asset('assets/images/logo.png')),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Hey ${auth.loggedUser?.username} $emoji',
-                              style: const TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
+                  Positioned(
+                    top: 80,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      width: size.width,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Palette.white,
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: const Offset(0, 1),
+                                    color:
+                                        Palette.primaryGreen.withOpacity(0.1),
+                                    blurRadius: 4),
+                              ],
                             ),
-                            const Text('Let\'s play some golf today!',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                )),
-                          ],
-                        ),
-                        CircleAvatar(
-                          radius: 26,
-                          child: CircleAvatar(
-                              radius: 24,
-                              foregroundColor: Colors.transparent,
-                              backgroundColor: Palette.white,
-                              child: Image.asset('assets/images/logo.png')),
-                        )
-                      ],
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                      height: 50,
+                                      width: 50,
+                                      child: Image.asset(
+                                          'assets/icons/courses.png')),
+                                  Text(
+                                    'Courses',
+                                    style: TextStyle(color: Palette.black),
+                                  )
+                                ]),
+                          ),
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Palette.white,
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: const Offset(0, 1),
+                                    color:
+                                        Palette.primaryGreen.withOpacity(0.1),
+                                    blurRadius: 4),
+                              ],
+                            ),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                      height: 50,
+                                      width: 50,
+                                      child: Image.asset(
+                                          'assets/icons/handicap.png')),
+                                  Text(
+                                    'Handicap',
+                                    style: TextStyle(color: Palette.black),
+                                  )
+                                ]),
+                          ),
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Palette.white,
+                              boxShadow: [
+                                BoxShadow(
+                                    offset: const Offset(0, 1),
+                                    color:
+                                        Palette.primaryGreen.withOpacity(0.1),
+                                    blurRadius: 4),
+                              ],
+                            ),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                      height: 50,
+                                      width: 50,
+                                      child:
+                                          Image.asset('assets/icons/shop.png')),
+                                  Text(
+                                    'Shop history',
+                                    style: TextStyle(color: Palette.black),
+                                  )
+                                ]),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ),
-                Positioned(
-                  top: 80,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    width: size.width,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 100,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Palette.white,
-                            boxShadow: [
-                              BoxShadow(
-                                  offset: const Offset(0, 1),
-                                  color: Palette.primaryGreen.withOpacity(0.1),
-                                  blurRadius: 4),
-                            ],
-                          ),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                    height: 50,
-                                    width: 50,
-                                    child: Image.asset(
-                                        'assets/icons/courses.png')),
-                                Text(
-                                  'Courses',
-                                  style: TextStyle(color: Palette.borderGreen),
-                                )
-                              ]),
-                        ),
-                        Container(
-                          height: 100,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Palette.white,
-                            boxShadow: [
-                              BoxShadow(
-                                  offset: const Offset(0, 1),
-                                  color: Palette.primaryGreen.withOpacity(0.1),
-                                  blurRadius: 4),
-                            ],
-                          ),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                    height: 50,
-                                    width: 50,
-                                    child: Image.asset(
-                                        'assets/icons/handicap.png')),
-                                Text(
-                                  'Handicap',
-                                  style: TextStyle(color: Palette.borderGreen),
-                                )
-                              ]),
-                        ),
-                        Container(
-                          height: 100,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Palette.white,
-                            boxShadow: [
-                              BoxShadow(
-                                  offset: const Offset(0, 1),
-                                  color: Palette.primaryGreen.withOpacity(0.1),
-                                  blurRadius: 4),
-                            ],
-                          ),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                    height: 50,
-                                    width: 50,
-                                    child:
-                                        Image.asset('assets/icons/shop.png')),
-                                Text(
-                                  'Shop history',
-                                  style: TextStyle(color: Palette.borderGreen),
-                                )
-                              ]),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ]),
-            ),
-            const SizedBox(height: 10),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text('Courses'),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Container(
-                width: size.width,
-                child: ListView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 3,
-                    itemBuilder: (context, index) => Container(
-                          height: 150,
-                          width: 150,
-                          decoration:
-                              BoxDecoration(color: Palette.primaryGreen),
-                        )),
+                  )
+                ]),
               ),
-            )
-          ],
+              const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text('Courses'),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Container(
+                  height: 180,
+                  width: size.width,
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 3,
+                      itemBuilder: (context, index) => Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: 170,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                color: Palette.secondaryColor
+                                    .withOpacity(0.4)
+                                    .withOpacity(0.1),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Column(
+                                children: [
+                                  ClipRRect(
+                                    child: Image.asset(
+                                        'assets/images/golf${index + 1}.png'),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
