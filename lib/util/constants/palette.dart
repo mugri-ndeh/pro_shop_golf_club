5b533b2ea070aa285f13388ec50c5d1ebb4dfc84
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-04 13:25:54 +0100

    color name changed

--- lib/util/constants/palette.dart
+++ lib/util/constants/palette.dart
@@ -5,6 +5,6 @@ class Palette {
   static Color secondaryColor = Color.fromARGB(255, 7, 221, 7);
 
   static Color scaffoldBg = const Color(0XFFF9F9F9);
-  static Color borderGreen = const Color.fromARGB(255, 0, 17, 8);
+  static Color black = const Color.fromARGB(255, 0, 17, 8);
   static Color white = Colors.white;
 }
