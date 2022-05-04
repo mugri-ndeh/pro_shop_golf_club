a8646d559614413313185b8ef99b32546904f895
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-04 13:24:50 +0100

    fixed profile

--- lib/home/ui/profile.dart
+++ lib/home/ui/profile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pro_shop_golf_club/auth/auth_provider.dart';
-import 'package:pro_shop_golf_club/home/util/widgets.dart';
+import 'package:pro_shop_golf_club/home/ui/widgets/widgets.dart';
 import 'package:pro_shop_golf_club/util/constants/palette.dart';
 import 'package:provider/provider.dart';
 
@@ -16,187 +16,194 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              // alignment: Alignment.center,
+      body: Consumer<Authentication>(
+        builder: (_, auth, __) => SingleChildScrollView(
+          child: SizedBox(
+            height: size.height,
+            child: Stack(
+              alignment: AlignmentDirectional.topCenter,
               children: [
                 Positioned(
-                    child: Container(
-                  height: 300,
-                  decoration: BoxDecoration(color: Palette.scaffoldBg),
-                )),
-                Positioned(
-                    child: Container(
-                  height: 250,
-                  decoration: BoxDecoration(
-                      color: Palette.primaryGreen,
-                      borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(30),
-                          bottomRight: Radius.circular(30))),
-                )),
-                Positioned(
-                  left: size.width * 0.25,
-                  right: size.width * 0.25,
                   top: 50,
-                  child: SizedBox(
-                    width: size.width * 0.5,
-                    child: const Center(
-                      child: Text(
-                        "Profile",
-                        style: TextStyle(fontSize: 34, color: Colors.white),
+                  child: Container(
+                    height: size.height,
+                    width: size.width * 0.9,
+                    decoration: decoration,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 70.0),
+                      child: Column(
+                        children: [
+                          Text(
+                              '${auth.loggedUser!.firstName} ${auth.loggedUser!.lastName}',
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          Text(
+                            '@${auth.loggedUser!.username}',
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 8.0, vertical: 4.0),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16.0, vertical: 8),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.shade200,
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    children: const [
+                                      Text(
+                                        '95',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text('Avg score'),
+                                    ],
+                                  ),
+                                  const CustomVerticalDivider(height: 25),
+                                  Column(
+                                    children: const [
+                                      Text('21',
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      Text('Rounds'),
+                                    ],
+                                  ),
+                                  const CustomVerticalDivider(height: 25),
+                                  Column(
+                                    children: const [
+                                      Text('28',
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      Text('Handicap'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 2),
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text('User information')),
+                          ),
+                          const SizedBox(height: 4),
+                          const CustomHorizontalDivider(
+                            padding: 8,
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.phone_outlined,
+                              color: Palette.black,
+                            ),
+                            title: Text(
+                              auth.loggedUser!.phoneNumber,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.mail_outline,
+                              color: Palette.black,
+                            ),
+                            title: Text(
+                              auth.loggedUser!.email,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.calendar_month_outlined,
+                              color: Palette.black,
+                            ),
+                            title: Text(
+                              auth.loggedUser!.dob,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text('Other information')),
+                          ),
+                          const SizedBox(height: 4),
+                          const CustomHorizontalDivider(
+                            padding: 8,
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.wallet_membership_outlined,
+                              color: Palette.black,
+                            ),
+                            title: Text('Membership'),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.help_outline,
+                              color: Palette.black,
+                            ),
+                            title: Text(
+                              'Help',
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.privacy_tip_outlined,
+                              color: Palette.black,
+                            ),
+                            title: Text(
+                              'Privacy Policy',
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.document_scanner_outlined,
+                              color: Palette.black,
+                            ),
+                            title: Text(
+                              'Terms of use',
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
-                Consumer<Authentication>(
-                  builder: (_, auth, __) => Positioned(
-                      bottom: 0,
-                      left: size.width * 0.075,
-                      right: size.width * 0.075,
-                      child: Container(
-                        height: 200,
-                        width: size.width * 0.85,
-                        decoration: decoration,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              CircleAvatar(
-                                radius: 50,
-                                backgroundColor: Palette.primaryGreen,
-                                child: const Icon(Icons.person),
-                              ),
-                              Text(
-                                auth.loggedUser!.username,
-                                style: const TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
-                              ),
-                              Text(auth.loggedUser!.phoneNumber)
-                            ]),
-                      )),
+                Positioned(
+                  top: 0,
+                  child: CircleAvatar(
+                    backgroundColor: Palette.secondaryColor
+                        .withOpacity(0.4)
+                        .withOpacity(0.1),
+                    radius: 50,
+                    child: Image.asset(
+                      'assets/images/golf1.png',
+                    ),
+                  ),
                 ),
               ],
             ),
-            const SizedBox(height: 20),
-            Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                ),
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: decoration,
-                        child: const ListTile(
-                          selected: true,
-                          selectedTileColor: Colors.white,
-                          leading: CircleAvatar(child: Icon(Icons.edit)),
-                          title: Text("Edit"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: decoration,
-                        child: const ListTile(
-                          selected: true,
-                          selectedTileColor: Colors.white,
-                          leading: CircleAvatar(child: Icon(Icons.privacy_tip)),
-                          title: Text("Privacy Policy"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: decoration,
-                        child: const ListTile(
-                          selected: true,
-                          selectedTileColor: Colors.white,
-                          leading: CircleAvatar(child: Icon(Icons.help)),
-                          title: Text("About"),
-                          trailing: Icon(Icons.arrow_forward_ios),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Consumer<Authentication>(
-                      builder: (_, auth, __) => GestureDetector(
-                        onTap: () {
-                          showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                    content: Text(
-                                        'Are you sure you want to logout?'),
-                                    actions: [
-                                      CustomButton(
-                                        onTap: () {
-                                          auth.logout();
-                                          Navigator.pop(context);
-                                        },
-                                        text: 'Yes',
-                                        color: Colors.red,
-                                      ),
-                                      CustomButton(
-                                          onTap: () {
-                                            Navigator.pop(context);
-                                          },
-                                          text: 'No'),
-                                    ],
-                                  ));
-                        },
-                        child: Container(
-                          padding: EdgeInsets.all(8),
-                          decoration: decoration,
-                          child: const ListTile(
-                            selected: true,
-                            selectedTileColor: Colors.white,
-                            leading: CircleAvatar(
-                              child: Icon(
-                                Icons.logout_rounded,
-                                size: 16,
-                                color: Colors.white,
-                              ),
-                              backgroundColor: Colors.red,
-                            ),
-                            title: Text(
-                              "Logout",
-                              style: TextStyle(color: Colors.red),
-                            ),
-                            trailing: Icon(Icons.arrow_forward_ios),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ))
-          ],
+          ),
         ),
       ),
     );
   }
 
   var decoration = BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.circular(10),
+    color: Palette.white,
+    borderRadius: BorderRadius.circular(20),
     boxShadow: [
       BoxShadow(
-        offset: Offset(1, 2),
+        offset: Offset(2, 4),
         blurStyle: BlurStyle.outer,
-        blurRadius: 3,
-        spreadRadius: 1,
+        blurRadius: 10,
+        spreadRadius: 3,
         color: Color.fromARGB(255, 228, 222, 222).withOpacity(0.2),
       ),
     ],
